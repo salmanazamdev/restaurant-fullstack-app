3cbdfb5f917408d64aed520edea8044a66307423
SalmanAzamDev <salmankhach@gmail.com>
2025-08-08 11:00:48 +0500

    Removed address column from users table

--- backend/database-schema.sql
+++ backend/database-schema.sql
@@ -4,7 +4,6 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    address TEXT, -- can be added/updated later
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP DEFAULT NULL
